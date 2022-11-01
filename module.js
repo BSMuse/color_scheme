a666be002966d65e54f7ae375eaa3620f55b965f
bsmuse <bsmuse@tutamail.com>
2022-10-31 18:44:20 -0600

    Complete application

--- module.js
+++ module.js
@@ -1,20 +1,52 @@
-const colors = []
+let colors = ''
 const colorPick = document.getElementById('color-pick')
 const modePick = document.getElementById('dropdown-box')
 let colorValue = colorPick.value.replace('#', '')
 let modeValue = modePick.value
 
-document.getElementById('scheme-btn').addEventListener('click', ()=> {
-    console.log(modeValue)
+const setColors = ()=> {
+    for (let i = 0; i < colors.length; i++) {
+        document.getElementById(`hex-${i+2}`).textContent = colors[i].hex.value
+        document.getElementById(`color-${i+2}`).style.background = colors[i].hex.value
+    }
+    console.log(colors)
+}
+
+const getData = () => {
     fetch(`https://www.thecolorapi.com/scheme?hex=${colorValue}&mode=${modeValue}&count=4&format=json`)
     .then(res => res.json())
-    .then(data => console.log(data.colors))
+    .then(data => colors = data.colors)
+}
+
+colorPick.addEventListener('input', ()=> {
+    colorValue = colorPick.value.replace('#', '')
+    document.getElementById('hex-1').textContent = colorPick.value
+    document.getElementById('color-1').style.background = colorPick.value
+    colors = ''
+    getData()
 })
 
-colorPick.addEventListener('input', ()=> colorValue = colorPick.value.replace('#', ''))
+modePick.addEventListener('change', ()=> { 
+    modeValue = modePick.value
+    colors = ''
+    getData()
+}) 
 
-modePick.addEventListener('change', ()=> modeValue = modePick.value) 
+document.getElementById('scheme-btn').addEventListener('click', ()=> {
+    setColors()
+})
 
-// https://www.thecolorapi.com/scheme?hex=4a8830&mode=Monochrome-dark&count=4&format=json
+const copyToClipboard = (id) => {
+    const r = document.createRange()
+    r.selectNode(document.getElementById(id))
+    window.getSelection().removeAllRanges()
+    window.getSelection().addRange(r)
+    document.execCommand('copy')
+    window.getSelection().removeAllRanges()
+} 
 
-// `https://www.thecolorapi.com/scheme?hex=${colorValue}&mode=${modeValue}&count=4&format=json`
+for (let i = 0; i < 5; i++) {
+    document.getElementById(`tooltiptext-${i+1}`).addEventListener('click', () => {
+        copyToClipboard(`hex-${i+1}`)
+    })
+}
